ee360546412ab98fe441a61d2eb25075967dedd3
Rob Anderson <rob.anderson2@gmail.com>
2015-01-11 05:45:55 +0000

    Add full_title helper and update title/meta tag

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Discover Hockey"
+    assert_select "title", "Discover Hockey"
   end
 
   test "should get help" do
