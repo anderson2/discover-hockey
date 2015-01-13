15f74f04c3f75421a5b137e0a1764c7e4b8eaddb
Rob Anderson <rob.anderson2@gmail.com>
2015-01-13 05:06:03 +0000

    Add partials and named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  
 
-  get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
